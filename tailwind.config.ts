ec2e980ff2133968203b8598e3ff243e530a8971
janipang <pang.janista@gmail.com>
2024-08-17 21:56:10 +0700

    start project with next.js next-ui and next-theme configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,40 @@
 import type { Config } from "tailwindcss";
+const { nextui } = require("@nextui-org/react");
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    extend: {},
+    light: {},
+    dark: {
+      colors: {
+        primary: {
+          DEFAULT: "#BEF264",
+          foreground: "#000000",
+        },
+        focus: "#BEF264",
+      },
+    },
+    "fantasy-dark": {
+      extend: "dark",
+      colors: {
+        background: "#0D001A",
+        foreground: "#ffffff",
+        primary: {
+          50: "#3B096C",
+          100: "#520F83",
+          DEFAULT: "#DD62ED",
+          foreground: "#ffffff",
+        },
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()]
 };
 export default config;
