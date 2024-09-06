c0d1782d53501f8b23b5ac0062bcd7de78cc6495
janipang <pang.janista@gmail.com>
2024-09-06 17:42:22 +0700

    nested navbar clickable >> but render at the buttom

--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -13,67 +13,81 @@ import {
 } from "@/constant/navigate";
 import { Button } from "@nextui-org/button";
 import MenuItem from "@/type/MenuItem";
+import Image from "next/image";
 
 const NestedDropdown: React.FC<{ items: MenuItem[] }> = ({ items }) => {
   return (
-    <>
-      {items.map((item) => {
-        if (item.child.length === 0) {
-          return (
-            <Button key={`${item.id}`} variant="flat">
-              {item.name}
-            </Button>
-          );
-        } else {
-          return (
-            <Dropdown key={`${item.id} dropdown`}>
-              <DropdownTrigger>
-                <Button variant="flat">{item.name}</Button>
-              </DropdownTrigger>
-              <DropdownMenu aria-label={`${item.name} Menu`}>
-                {item.child.map((child) => (
-                  <DropdownItem key={`${child.id} choice`}>
-                    {child.child.length == 0 ? (
-                      child.name
-                    ) : (
-                      <Dropdown key={`${child.id} dropdown`}>
-                        <DropdownTrigger>
-                          <Button variant="flat">{child.name}</Button>
-                        </DropdownTrigger>
-                        <DropdownMenu aria-label={`${child.name} Menu`}>
-                          {child.child.map((grandch) => (
-                            <DropdownItem key={`${grandch.id} choice`}>
-                              {grandch.child.length == 0 ? (
-                                grandch.name
-                              ) : (
-                                <Dropdown key={`${child.id} dropdown`}>
-                                  <DropdownTrigger>
-                                    <Button variant="flat">
-                                      {grandch.name}
-                                    </Button>
-                                  </DropdownTrigger>
-                                  <DropdownMenu
-                                    aria-label={`${grandch.name} Menu`}
+    <div className="flex flex-row w-full py-2 px-4">
+      <Image src="/success.jpg" alt="logo" height={30} width={60} />
+      <nav className="flex flex-row gap-4">
+        {items.map((item) => {
+          if (item.child.length === 0) {
+            return (
+              <Button key={`${item.id}`} variant="flat">
+                {item.name}
+              </Button>
+            );
+          } else {
+            return (
+              <Dropdown key={`${item.id} dropdown`} closeOnSelect={false}>
+                <DropdownTrigger>
+                  <Button variant="flat">{item.name}</Button>
+                </DropdownTrigger>
+                <DropdownMenu aria-label={`${item.name} Menu`}>
+                  {item.child.map((child) => (
+                    <DropdownItem key={`${child.id} choice`}>
+                      {child.child.length == 0 ? (
+                        child.name
+                      ) : (
+                        <Dropdown 
+                          key={`${child.id} dropdown`}
+                          placement="bottom"
+                          closeOnSelect={false}
+                        >
+                          <DropdownTrigger>
+                            <button className="w-full outline-none flex flex-row justify-between">
+                              <p>{child.name}</p>
+                              <p>{">"}</p>
+                            </button>
+                          </DropdownTrigger>
+                          <DropdownMenu aria-label={`${child.name} Menu`}>
+                            {child.child.map((grandch) => (
+                              <DropdownItem key={`${grandch.id} choice`}>
+                                {grandch.child.length == 0 ? (
+                                  grandch.name
+                                ) : (
+                                  <Dropdown
+                                    key={`${child.id} dropdown`}
+                                    placement="bottom"
+                                    closeOnSelect={false}
                                   >
-                                    <>
-                                    {/* will be implemented */}
-                                    </>
-                                  </DropdownMenu>
-                                </Dropdown>
-                              )}
-                            </DropdownItem>
-                          ))}
-                        </DropdownMenu>
-                      </Dropdown>
-                    )}
-                  </DropdownItem>
-                ))}
-              </DropdownMenu>
-            </Dropdown>
-          );
-        }
-      })}
-    </>
+                                    <DropdownTrigger>
+                                      <button className="w-full outline-none flex flex-row justify-between">
+                                        <p>{grandch.name}</p>
+                                        <p>{">"}</p>
+                                      </button>
+                                    </DropdownTrigger>
+                                    <DropdownMenu
+                                      aria-label={`${grandch.name} Menu`}
+                                    >
+                                      <>{/* will be implemented */}</>
+                                    </DropdownMenu>
+                                  </Dropdown>
+                                )}
+                              </DropdownItem>
+                            ))}
+                          </DropdownMenu>
+                        </Dropdown>
+                      )}
+                    </DropdownItem>
+                  ))}
+                </DropdownMenu>
+              </Dropdown>
+            );
+          }
+        })}
+      </nav>
+    </div>
   );
 };
 
