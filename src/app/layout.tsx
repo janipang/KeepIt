b025a009377b529d93864975ac893acb97ceabdb
janipang <pang.janista@gmail.com>
2024-09-17 16:07:34 +0700

    chore(font): default font noto sans thai

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Noto_Sans_Thai } from "next/font/google";
 import "./globals.css";
 import {Providers} from "./providers";
 
-const inter = Inter({ subsets: ["latin"] });
+const noto = Noto_Sans_Thai({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
+});
 
 export const metadata: Metadata = {
   title: "Keep It Simple",
@@ -16,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={inter.className}>
+    <html lang="en" className={noto.className}>
       <body>
         <Providers>
           {children}
