35b5a6760f92a53a3589438e5f7bd730640146fc
janipang <pang.janista@gmail.com>
2024-09-18 00:06:08 +0700

    (feat)contact: add select box

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,33 +2,40 @@
 import { PlusIcon } from "@/components/icons";
 import SearchInput from "@/components/searchInput";
 import ContactTable from "@/components/table/contactTable";
-import { Accordion, AccordionItem } from "@nextui-org/accordion";
+import { Listbox, ListboxItem, cn } from "@nextui-org/react";
 import { Button } from "@nextui-org/button";
+import { useMemo, useState } from "react";
 
 export default function Contact() {
-  const defaultContent =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    const [selectedKeys, setSelectedKeys] = useState(new Set(["text"]));
+  
+    const selectedValue = useMemo(
+      () => Array.from(selectedKeys).join(", "),
+      [selectedKeys]
+    );
   return (
-    <div className="p-4 rounded-lg shadow-lg grid gap-2 grid-cols-[1fr_5fr] grid-rows-[50px_1fr] bg-white">
+    <div className="p-4 rounded-lg shadow-lg grid gap-2 grid-cols-[1fr_6fr] grid-rows-[50px_1fr] bg-white">
       <span>ผู้ติดต่อ</span>
       <Button startContent={<PlusIcon />} className="w-fit">
         เพิ่มผู้ติดต่อ
       </Button>
-      <Accordion selectionMode="multiple">
-        <AccordionItem key="1" aria-label="ทั้งหมด1" title="ทั้งหมด">
-          {defaultContent}
-        </AccordionItem>
-        <AccordionItem key="2" aria-label="ลูกค้า" title="ลูกค้า">
-          {defaultContent}
-        </AccordionItem>
-        <AccordionItem key="3" aria-label="ผู้ขาย" title="ผู้ขาย">
-          {defaultContent}
-        </AccordionItem>
-        <AccordionItem key="4" aria-label="ปิดใช้งาน" title="ปิดใช้งาน">
-          {defaultContent}
-        </AccordionItem>
-      </Accordion>
-        <ContactTable />
+      <div className="flex w-full flex-col">
+        <div className="w-full max-w-[260px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100">
+          <Listbox
+            aria-label="Single selection example"
+            variant="flat"
+            disallowEmptySelection
+            selectionMode="single"
+            selectedKeys={selectedKeys}
+            onSelectionChange={() => setSelectedKeys}
+          >
+            <ListboxItem key="all" className="p-4" showDivider>ทั้งหมด</ListboxItem>
+            <ListboxItem key="customer" className="p-4" showDivider>ลูกค้า</ListboxItem>
+            <ListboxItem key="provider" className="p-4">ผู้ขาย</ListboxItem>
+          </Listbox>
+        </div>
+      </div>
+      <ContactTable />
     </div>
   );
 }
